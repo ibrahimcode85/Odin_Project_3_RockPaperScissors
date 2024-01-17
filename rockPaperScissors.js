0ba5b234ffb924d2f9dfc262bb79f44b70f82613
ibrahim_ahmad <ibrahimahmad.info@gmail.com>
2024-01-17 18:14:38 +0800

    Add UI to the rock paper scissors game.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -71,73 +71,59 @@ function playRound(playerSelection, computerSelection) {
 }
 
 // create a best-of-five game
-function game() {
-    // declare and initialized score variable
-    let score = 0;
-    let game = 1;
-
-    // play game times
-    playerSelection = prompt(`Please select rock, paper or scissors for game #${game}.`)
+function game(playerSelection) {
+    
     computerSelection = getComputerChoice();                    // update computer selection
     textResult = playRound(playerSelection, computerSelection); // play the game
-    checkResult = textResult.includes('won');                   // check if player had won
     
-    if (checkResult) {score = score + 1};                       // update score if won
-
-    console.log(`Game #${game}: Player select: ${playerSelection}; Computer select: ${computerSelection}. Score: ${score} out of 5.`);
-
-    game = game + 1;
-
-    // play game four more times (not using loop at this stage of lesson.)
-    // game #2 ------------------------------------------------------------------------------
-    playerSelection = prompt(`Please select rock, paper or scissors for game #${game}.`)
-    computerSelection = getComputerChoice();                    // update computer selection
-    textResult = playRound(playerSelection, computerSelection); // play the game
-    checkResult = textResult.includes('won');                   // check if player had won
-    
-    if (checkResult) {score = score + 1};                       // update score if won
-
-    console.log(`Game #${game}: Player select: ${playerSelection}; Computer select: ${computerSelection}. Score: ${score} out of 5.`);
-
-    game = game + 1;
-
-    // game #3 ------------------------------------------------------------------------------
-    playerSelection = prompt(`Please select rock, paper or scissors for game #${game}.`)
-    computerSelection = getComputerChoice();                    // update computer selection
-    textResult = playRound(playerSelection, computerSelection); // play the game
-    checkResult = textResult.includes('won');                   // check if player had won
-    
-    if (checkResult) {score = score + 1};                       // update score if won
-
-    console.log(`Game #${game}: Player select: ${playerSelection}; Computer select: ${computerSelection}. Score: ${score} out of 5.`);
-
-    game = game + 1;
-
-    // game #4 ------------------------------------------------------------------------------
-    playerSelection = prompt(`Please select rock, paper or scissors for game #${game}.`)
-    computerSelection = getComputerChoice();                    // update computer selection
-    textResult = playRound(playerSelection, computerSelection); // play the game
-    checkResult = textResult.includes('won');                   // check if player had won
-    
-    if (checkResult) {score = score + 1};                       // update score if won
-
-    console.log(`Game #${game}: Player select: ${playerSelection}; Computer select: ${computerSelection}. Score: ${score} out of 5.`);
-
-    game = game + 1;
-
-    // game #5 ------------------------------------------------------------------------------
-    playerSelection = prompt(`Please select rock, paper or scissors for game #${game}.`)
-    computerSelection = getComputerChoice();                    // update computer selection
-    textResult = playRound(playerSelection, computerSelection); // play the game
-    checkResult = textResult.includes('won');                   // check if player had won
-    
-    if (checkResult) {score = score + 1};                       // update score if won
-
-    console.log(`Game #${game}: Player select: ${playerSelection}; Computer select: ${computerSelection}. Score: ${score} out of 5.`);
-
-    game = game + 1;
-
+    return arrayResult = [playerSelection, computerSelection, textResult]
 }
 
-// run the game function
-game();
+// run the game
+const clickChoices = document.querySelector('#buttons');
+const divResult = document.querySelector('#result');
+
+var gameNumber = 0;
+var score = 0;
+
+const clickHandler = (e) => {
+    gameNumber = gameNumber + 1;
+
+    let playerSelection = e.target.getAttribute('id').toUpperCase();
+    let arrayResult = game(playerSelection);        // arrayResult = [playerSelection, computerSelection, textResult]
+    let computerSelect = arrayResult[1].toUpperCase();
+    let textResult = arrayResult[2];
+
+    // calculate score
+    checkResult = textResult.includes('won');       // check if player had won
+    if (checkResult) {score = score + 1};           // update score if won
+
+    // print result text
+    printResult = `Game number ${gameNumber}: Player selected ${playerSelection} while computer selected ${computerSelect}. "${textResult}". Current score is ${score} out of ${gameNumber}`;
+    const pResult = document.createElement('p');
+    pResult.textContent = printResult;
+    divResult.appendChild(pResult);
+
+   // process end of game
+   if (gameNumber >= 5) {
+    const finalResult = document.createElement('h3');
+
+    if (score >= 3) {
+        finalResult.textContent = `Congratulation you have won! You have won ${score} out of 5 games played.`
+    } else{
+        finalResult.textContent = `You have lost! You have only won ${score} out of 5 games played.`
+    };
+
+    finalResult.setAttribute('id', 'finalResult');
+    document.body.appendChild(finalResult);
+    endGame();
+    };
+};
+
+clickChoices.addEventListener('click', clickHandler);
+
+function endGame() {
+    clickChoices.removeEventListener('click', clickHandler);
+    setTimeout(() => { alert('This is the end of Game. Please reload the page to play the game again.'); }, 1000);
+    
+}
